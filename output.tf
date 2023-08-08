a1a0757a0ecfe1762ea560592a3a6d6e4fb5c794
Samuel7050 <109923020+Samuel7050@users.noreply.github.com>
2023-08-08 12:59:43 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,1 +1,14 @@
+output "bucket_arn" {
+  description = "s3 bucket_arn"
+  value       = aws_s3_bucket.bucket.arn
+}
 
+output "bucket_name" {
+  description = "s3 bucketid"
+  value       = aws_s3_bucket.bucket.id
+}
+
+output "bucket_domain" {
+  description = "s3 bucket_domain"
+  value       = aws_s3_bucket_website_configuration.mytfbucket_website_configuration.website_domain
+}
