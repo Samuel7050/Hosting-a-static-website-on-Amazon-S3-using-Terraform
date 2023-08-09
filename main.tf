0a89f1ace2fc7b762b3946db797670f17bbcbe8e
oluwagbenga <ojosamuel700@gmail.com>
2023-08-09 10:47:02 +0100

    Format

--- main.tf
+++ main.tf
@@ -20,11 +20,11 @@ resource "aws_s3_bucket_policy" "public_access_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid = "PublicReadGetObject",
-        Effect = "Allow",
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
         Principal = "*",
-        Action = "s3:GetObject",
-        Resource = "${aws_s3_bucket.tf_bucket.arn}/*"
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.tf_bucket.arn}/*"
       }
     ]
   })
@@ -64,5 +64,5 @@ resource "aws_s3_object" "file" {
   bucket = aws_s3_bucket.tf_bucket.id
   key    = "index.html"
   source = "index.html"
-  acl = "public-read"
+  acl    = "public-read"
 }
--- output.tf
+++ output.tf
@@ -16,4 +16,4 @@ output "bucket_domain" {
 
 output "website_url" {
   value = "http://${aws_s3_bucket.tf_bucket.bucket}.s3-website.${var.region}.amazonaws.com"
-  }
+}
